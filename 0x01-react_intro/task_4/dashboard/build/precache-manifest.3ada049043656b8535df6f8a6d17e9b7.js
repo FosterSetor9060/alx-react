4ea0361edb40fcc78f98a9719cd2fe2e6495df87
Foster Setor <walkerdziwornu@gmail.com>
2024-08-27 08:27:01 +0000

    myReactIntroTask

---
+++ 0x01-react_intro/task_4/dashboard/build/precache-manifest.3ada049043656b8535df6f8a6d17e9b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a813cb76209fc6ec7d40ecc6777c8875",
+    "url": "%3Cyour-github-username%3E.github.io/alx-react/0x01-react_intro/task_4/dashboard/index.html"
+  },
+  {
+    "revision": "aea54c2cc0e9daa36ce6",
+    "url": "%3Cyour-github-username%3E.github.io/alx-react/0x01-react_intro/task_4/dashboard/static/js/2.bbc244fd.chunk.js"
+  },
+  {
+    "revision": "e2a960147ef5362ced8a",
+    "url": "%3Cyour-github-username%3E.github.io/alx-react/0x01-react_intro/task_4/dashboard/static/js/3.1b3c587a.chunk.js"
+  },
+  {
+    "revision": "af7299cc0bd51bf5ace7",
+    "url": "%3Cyour-github-username%3E.github.io/alx-react/0x01-react_intro/task_4/dashboard/static/js/main.9f0a7633.chunk.js"
+  },
+  {
+    "revision": "752f15ed70d52b320208",
+    "url": "%3Cyour-github-username%3E.github.io/alx-react/0x01-react_intro/task_4/dashboard/static/js/runtime~main.7b844325.js"
+  }
+]);

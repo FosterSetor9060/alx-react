50e5fb7028f2d05e49f8d37a9668bc3cf23fb242
Foster setor <walkerdziwornu@gmail.com>
2024-09-11 09:34:05 +0000

    myReactImmutableTask

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -1,8 +1,7 @@
 import { fromJS } from 'immutable';
 
 export default function accessImmutableObject(object, array) {
-  const immutableObject = fromJS(object);
+  const mappedObj = fromJS(object);
 
-  return immutableObject.getIn(array);
+  return mappedObj.getIn(array, undefined);
 }
-

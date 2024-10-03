02e05095c45867268cb6a224cb3dbf30f021a5b6
Foster Setor <walkerdziwornu@gmail.com>
2024-10-03 06:37:46 +0000

    myReactTaskReactComponet

--- 0x03-React_component/task_1/dashboard/src/App/App.js
+++ 0x03-React_component/task_1/dashboard/src/App/App.js
@@ -1,40 +1,10 @@
 import React from 'react';
-import './App.css';
-import Header from '../Header/Header';
-import Footer from '../Footer/Footer';
-import Notification from '../Notifications/Notifications';
-import Login from '../Login/Login';
-import CourseList from '../CourseList/CourseList';
-import { getLatestNotification } from '../utils/utils';
 import PropTypes from 'prop-types';
 
-
 class App extends React.Component {
-  static listCourses = [
-    {id: 1, name: 'ES6', credit: 60},
-    {id: 2, name: 'Webpack', credit: 20},
-    {id: 3, name: 'React', credit: 40}
-  ];
-
-  static listNotifications = [
-    {id: 1, value: "New course available", type: "default"},
-    {id: 2, value: "New resume available", type: "urgent"},
-    {id: 3, html: {__html: getLatestNotification()}, type: "urgent"},
-  ];
-
   constructor(props) {
     super(props);
-    this.isLoggedIn = props.isLoggedIn;
-    this.logOut = props.logOut;
     this.handleKeyDown = this.handleKeyDown.bind(this);
-  }
-
-  handleKeyDown(e) {
-    e.preventDefault();
-    if (e.ctrlKey && e.key === 'h') {
-        alert("Logging you out");
-        this.logOut();
-    }  
   }
 
   componentDidMount() {
@@ -45,28 +15,31 @@ class App extends React.Component {
     window.removeEventListener('keydown', this.handleKeyDown);
   }
 
-  render () {
+  handleKeyDown(event) {
+    const { logOut } = this.props;
+    if (event.ctrlKey && event.key === 'h') {
+      alert('Logging you out');
+      logOut();
+    }
+  }
+
+  render() {
+    // The rest of your render logic
     return (
-      <React.Fragment>
-        <Notification listNotifications={this.listNotifications}/>
-        <div className="App">
-          <Header />
-          {this.props.isLoggedIn ? <CourseList listCourses={this.listCourses}/> : <Login />}
-          <Footer />
-        </div>
-      </React.Fragment>
+      <div>
+        {/* Your component JSX */}
+      </div>
     );
   }
 }
 
-App.defaultProps = {
-  isLoggedIn: false,
-  logOut: () => {}
+App.propTypes = {
+  logOut: PropTypes.func,
 };
 
-App.propTypes = {
-  isLoggedIn: PropTypes.bool,
-  logOut: PropTypes.func
+App.defaultProps = {
+  logOut: () => {},
 };
 
 export default App;
+
